fb6dd2593f280f51685f8657910777210092759b
tugbauysalar <uysalartugba@gmail.com>
2023-12-06 17:19:04 +0300

    ürün için entity oluşturuldu

---
+++ Core/BookstoreProject.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Mime;
+
+namespace BookstoreProject.Domain.Entities;
+
+public class ProductEntity
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public decimal Stock { get; set; }
+    public int Id { get; set; }
+    
+}
