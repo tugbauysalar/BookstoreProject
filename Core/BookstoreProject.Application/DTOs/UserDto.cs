4259018ff78e680dbf49ac8bdf9e1a613abd0424
tugbauysalar <uysalartugba@gmail.com>
2023-12-19 02:00:34 +0300

    userdto ve customresponsedto sınıfları oluşturuldu
    kullanıcı bilgilerini içeren userdto sınıfı ve hatalı ve başarılı durum kodlarını dönen customresponsedto sınıfı oluşturuldu

---
+++ Core/BookstoreProject.Application/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookstoreProject.Application.DTOs;
+
+public class UserDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    
+}
